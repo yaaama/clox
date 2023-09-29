fd9773acaa4a2f59385202c5fcc18f1a5962a479
Sau P <sau@yaaama.xyz>
2023-09-29 13:46:19 +0100

    AST structures defined

--- src/ast.c
+++ src/ast.c
@@ -1,4 +1,5 @@
 #include "include/ast.h"
+#include <stdio.h>
 
 AST_t *ast_create(AST_Type type) {
 
@@ -11,4 +12,24 @@ AST_t *ast_create(AST_Type type) {
   }
 
   return ast;
+}
+
+// Function to pretty print an AST node and its children
+void pretty_print_ast(AST_t *node, int depth) {
+  if (node == NULL) {
+    return;
+  }
+
+  // Print indentation based on the depth of the node
+  for (int i = 0; i < depth; i++) {
+    printf("  ");
+  }
+
+  // Print the node's name and type
+  printf("NAME: %s , TYPE: (%d)\n", node->name, node->type);
+
+  // Recursively print the children of the node
+  for (size_t i = 0; i < node->children->size; i++) {
+    pretty_print_ast(node->children->items[i], depth + 1);
+  }
 }
--- src/include/ast.h
+++ src/include/ast.h
@@ -2,6 +2,7 @@
 #define AST_H_
 
 #include "list.h"
+#include "token.h"
 
 /* AST Types */
 /* #define FOREACH_AST_TYPE(AST_TYPE) \ */
@@ -58,7 +59,90 @@ typedef struct AST_STRUCT {
 
 } AST_t;
 
+// NOTE:   "Assign   : Token name, Expr value",
+typedef struct {
+  AST_t *expr;
+  Token *name;
+  AST_t *value;
+} Assign;
+
+// NOTE: "Binary   : Expr left, Token operator, Expr right",
+typedef struct {
+  AST_t expr;
+  AST_t *left;
+  Token *operator;
+  AST_t *right;
+} Binary;
+
+// NOTE:  "Call     : Expr callee, Token paren, List<Expr> arguments",
+typedef struct {
+  AST_t expr;
+  AST_t *callee;
+  Token *paren; // token for the closing parenthesis; its location is used when
+                // reporting a runtime error
+  AST_t *arguments; // List of expressions
+} Call;
+
+// NOTE:  "Get      : Expr object, Token name",
+typedef struct {
+  AST_t expr;
+  AST_t *object;
+  Token *name;
+} Get;
+
+// NOTE:  "Grouping : Expr expression",
+typedef struct {
+  AST_t expr;
+  AST_t *expression;
+} Grouping;
+
+// NOTE:  "Literal  : Object value",
+typedef struct {
+  AST_t expr;
+  Token value;
+} Literal;
+
+// NOTE:  "Logical  : Expr left, Token operator, Expr right",
+typedef struct {
+  AST_t expr;
+  AST_t *left;
+  Token *operator;
+  AST_t *right;
+} Logical;
+
+// NOTE: "Unary    : Token operator, AST_t right",
+typedef struct {
+  AST_t AST_t;
+  Token *operator;
+  AST_t *right;
+} Unary;
+
+// NOTE: "Variable : Token name"
+typedef struct {
+  AST_t expr;
+  Token *name;
+} Variable;
+
 AST_t *ast_create(AST_Type type);
-char *ast_type_to_str(AST_Type type);
+char *ast_type_to_str(AST_Type type); // TODO
+
+void pretty_print_ast(AST_t *node, int depth);
+
+AST_t *ast_assign(Token *name, AST_t *value);
+AST_t *ast_binary(AST_t *left, Token *operator, AST_t * right);
+AST_t *ast_call(AST_t *callee, Token *paren, AST_t *arguments);
+AST_t *ast_literal(Token *value);
+AST_t *ast_logical(AST_t *left, Token *operator, AST_t * right);
+AST_t *ast_unary(Token *operator, AST_t * right);
+AST_t *ast_variable(Token *name);
+AST_t *ast_number_literal(double value);
+/* AST_t *init_set(AST_t *object, Token *name, AST_t *value); */
+/* AST_t *init_get(AST_t *object, Token *name); */
+/* AST_t *init_grouping(AST_t *expression); */
+/* AST_t *init_super(Token *keyword, Token *method); */
+/* AST_t *init_this(Token *keyword); */
+/* AST_t *init_bool_literal(bool value); */
+/* AST_t *init_nil_literal(void); */
+/* void free_expr(AST_t *AST); */
 
 #endif // AST_H_
